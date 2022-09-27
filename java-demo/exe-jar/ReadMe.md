cfc70ce6882c65241c6004b4b397d781e4499546
DESKTOP-POF99M7\August <zjzouxu@gmail.com>
2022-09-27 19:47:02 +0800

    add exe-jar module

---
+++ java-demo/exe-jar/ReadMe.md
@@ -0,0 +1,5 @@
+### 参考文档
+
+[How to Create an Executable JAR with Maven](https://www.baeldung.com/executable-jar-with-maven)
+
+[maven-assembly-plugin](https://maven.apache.org/plugins/maven-assembly-plugin/index.html)
---
+++ java-demo/exe-jar/src/main/java/cn/axu/App.java
@@ -0,0 +1,12 @@
+package cn.axu;
+
+/**
+ * @Author: August Zou
+ * @Date: 2022/9/26 13:52
+ * @Description:
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Exe Jar");
+    }
+}
